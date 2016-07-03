b4e8c847e99415ff3134cc4be458a01147ae397d
Mosen <mosen@users.noreply.github.com>
2016-07-03 16:45:55 +1000

    Add babel config to make mocha babel transpiler work. Add npm test runscript Add a faux test to make sure the configuration is fine.

---
+++ test/components/HeaderTest.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render, mount, shallow} from 'enzyme';
+import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+
+chai.use(chaiEnzyme());
+
+describe('<Header />', () => {
+  'use strict';
+  
+});
