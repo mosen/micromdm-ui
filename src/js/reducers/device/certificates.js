fc5e7051e74e839a0afc45a2329f9a4dcc3da9f1
Mosen <mosen@users.noreply.github.com>
2016-07-23 23:23:50 +1000

    Update the certificates reducer to include certificates management endpoint responses.

--- src/js/reducers/device/certificates.js
+++ src/js/reducers/device/certificates.js
@@ -1,7 +1,13 @@
 'use strict';
+import moment from 'moment';
 import * as ui from '../../actions/ui/device';
+import * as api from '../../actions/api/devices';
 
 const initialState = {
+  lastUpdated: null,
+  loading: false,
+  error: false,
+  errorDetails: {},
   items: [],
   expanded: false
 };
@@ -12,6 +18,31 @@ export default function certificates (state = initialState, action) {
       return {
         ...state,
         expanded: action.payload
+      };
+    case api.CERTS_INDEX_REQUEST:
+      if (action.error) {
+        return Object.assign({}, state, {
+          error: true,
+          errorDetails: action.payload
+        });
+      } else {
+        return Object.assign({}, state, {
+          loading: true
+        });
+      }
+    case api.CERTS_INDEX_FAILURE:
+      return {
+        ...state,
+        error: true,
+        errorDetails: action.payload,
+        loading: false
+      };
+    case api.CERTS_INDEX_SUCCESS:
+      return {
+        ...state,
+        lastUpdated: moment().utc(),
+        items: action.payload,
+        loading: false
       };
     default:
       return state;
